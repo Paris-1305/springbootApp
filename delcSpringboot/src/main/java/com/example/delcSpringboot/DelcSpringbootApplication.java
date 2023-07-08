7834b5f8be64d9e3e18ad55f533e76952cb2acb0
Boyom Steve <boyomsteve@gmail.com>
2023-07-08 08:01:08 -0700

    springapp

---
+++ delcSpringboot/src/main/java/com/example/delcSpringboot/DelcSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.delcSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelcSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelcSpringbootApplication.class, args);
+	}
+
+}
---
+++ delcSpringboot/src/test/java/com/example/delcSpringboot/DelcSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.delcSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelcSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
